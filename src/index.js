5ef9d16b78e9ab6dea60d5a48b52b058184e1c63
misha133kv <mihail_fomin_95@mail.ru>
2023-09-24 13:31:43 +0500

    added slice

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
