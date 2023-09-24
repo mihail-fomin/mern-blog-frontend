69e41c7e71a9d1313f88d2c78c363ee02a565fe3
misha133kv <mihail_fomin_95@mail.ru>
2023-09-24 20:04:50 +0500

    added fetch tags

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
-import { fetchPosts } from '../store/slices/posts'
+import { fetchPosts, fetchTags } from '../store/slices/posts'
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
@@ -16,6 +16,7 @@ export const Home = () => {
 
   React.useEffect(() => {
     dispatch(fetchPosts())
+    dispatch(fetchTags())
   }, [])
 
   return (
@@ -34,16 +35,11 @@ export const Home = () => {
                 id={post._id}
                 title={post.title}
                 imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-                user={{
-                  avatarUrl:
-                    'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                  fullName: 'Keff',
-                }}
-                createdAt={'12 июня 2022 г.'}
-                viewsCount={150}
+                user={post.user}
+                createdAt={post.createdAt}
+                viewsCount={post.viewsCount}
                 commentsCount={3}
-                tags={['react', 'fun', 'typescript']}
-                isLoading={true}
+                tags={post.tags}
                 isEditable
               />
             ))}
